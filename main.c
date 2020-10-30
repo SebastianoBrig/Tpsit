f88dc4d07aff1ca06bffb8a85f72a9d2fe490283
Sebastiano Brignone <71812452+SebastianoBrig@users.noreply.github.com>
2020-10-30 16:21:03 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,54 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX 100
-
-typedef struct s_canzone{
-	int  num;
-	char nome[20];
-	char autore[20];
-} canzone;
-
-void Memorizzazione(canzone* Playlist, FILE* ptr);
+#define MAX 1000
 
 int main()
 {
-    canzone Playlist[MAX];
     FILE *ptr;
-    ptr=fopen("playlist.csv", "rt");
-    if (ptr){
-            printf("file ok!\n");
-            Memorizzazione(Playlist,ptr);
-            fclose(ptr);
+    int ac=0;
+    int gc=0;
+    int tc=0;
+    int cc=0;
+    int c;
+    if((ptr=fopen("rna.txt", "r"))==NULL) {
+		printf("Errore nell'apertura del file'");
+		return 1;
+	}
 
-    return 0;
+    do {
+        //prelevo un carattere e controllo se e' uguale a uno dei caratteri da contare;
+		c=fgetc(ptr);
+		if (c=='a'){
+            ac++;
+		}
+		if (c=='g'){
+            gc++;
+		}
+		if (c=='c'){
+            cc++;
+		}
+		if (c=='t'){
+            tc++;
+		}
+	} while(c!=EOF);
+	printf("%d\n", ac);
+	printf("%d\n", gc);
+	printf("%d\n", cc);
+	printf("%d\n", tc);
+	fclose(ptr);
+	//stampo i contatori;
 
-    }
+	return 0;
+
+
 }
-void Memorizzazione(canzone* Playlist, FILE* ptr){
-
-            int i=0,k=0,w,j;
-            char vet[MAX];
-            fscanf(ptr, "%d,", &Playlist[i].num);
-
-            for (i=0;fgets(vet,100,ptr)!=NULL;i++){
-
-                for(w = 0; vet[w]!=','; w++){
-                    Playlist[i].nome[w]=vet[w];
-                    }
-            Playlist[i].nome[w] = '\0';
-            for(j = w+1; vet[j]!='\n'; j++){
-            Playlist[i].autore[k]= vet[j];
-            k++;
-        }
-        printf("%d\n", Playlist[i].num);
-        printf("%s\n", Playlist[i].nome);
-        printf("%s\n", Playlist[i].autore);
-
-        fscanf(ptr, "%d,", &Playlist[i+1].num);
-        k=0;
-
-            }
-
-        }
-
